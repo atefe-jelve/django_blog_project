c47546e3e3a80bb79ea60c259208b8e2af83dc00
atefe jelve <a.jelve.69@gmail.com>
2022-09-03 11:30:46 +0430

    configure for deploy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-7a49oll4w09$tslxnjgcmggiy=-)req73m6iavj7dul(&c!=&a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
